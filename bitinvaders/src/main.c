8506e1fe4e0903d0707135e8aeec385e154a661a
Andy Best <andybest.net@gmail.com>
2016-01-31 15:32:28 +0000

    Drawing invader

--- bitinvaders/src/main.c
+++ bitinvaders/src/main.c
@@ -4,11 +4,15 @@
 #include <stdio.h>
 
 Sprite *logo;
+Sprite *invader;
+
 bool right;
 bool down;
 
 void game_init() {
     logo = new_sprite_from_data(image_logo.pixels, image_logo.width, image_logo.height);
+    invader = new_sprite_from_data(image_invader1.pixels, image_invader1.width, image_invader1.height);
+
     right = false;
     down = true;
 }
@@ -54,6 +58,16 @@ void graph_line() {
     if(sprite_is_on_line(logo, currentLine)) {
         for(int x = 0; x < 320; x++) {
             uint16_t p = logo->drawable.draw_pixel(logo, x, currentLine);
+            if(p != NO_PIXEL) {
+                draw_buffer[(x*2)] = p;
+                draw_buffer[(x*2) + 1] = p;
+            }
+        }
+    }
+
+    if(sprite_is_on_line(invader, currentLine)) {
+        for(int x = 0; x < 320; x++) {
+            uint16_t p = invader->drawable.draw_pixel(invader, x, currentLine);
             if(p != NO_PIXEL) {
                 draw_buffer[(x*2)] = p;
                 draw_buffer[(x*2) + 1] = p;
